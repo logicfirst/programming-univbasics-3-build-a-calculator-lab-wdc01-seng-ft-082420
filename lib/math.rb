0dc726dfc6d720bdc569ed66c2badb771feefba1
Scott H Lee <slee@logicfirstllc.com>
2020-07-18 19:46:18 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,25 @@
 def addition(num1, num2)
-
+  calc_sum = num1 + num2
+  calc_sum
 end
 
 def subtraction(num1, num2)
-
+  calc_diff = num1 - num2
 end
 
 def division(num1, num2)
-
+  calc_div = num1 / num2
 end
 
 def multiplication(num1, num2)
-
+  calc_mult = num1 * num2
 end
 
 def modulo(num1, num2)
-
+  calc_mod = num1 % num2
 end
 
 def square_root(num)
-
+  calc_sqrrt = Math.sqrt(num)
 end
+
